770b5e6ce894a56c65c85fae1459f0aba0b02938
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2023-11-24 00:16:54 +0000

    Clean

--- index.js
+++ index.js
@@ -3,56 +3,79 @@ const fs = require('fs');
 
 let prefix = "!";
 
-function main() {
-  const client = new Client({
-      name: "CommanderBox",
-      lobbyCode: "LbCd3hrR"
+function setupClient() {
+  return new Client({
+    name: "CommanderBox",
+    lobbyCode: "LbCd3hrR"
   });
+}
 
+
+function registerEventHandlers(client) {
+  client.on("connect", () => handleConnect(client));
+  client.on("disconnect", (data) => handleDisconnect(data));
+  
   const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-  client.on("connect", () => {
-      console.log(`Connected to ${client.lobbyId}\nCurrent Player Count: ${client.players.length}`);
-    
-      client.sendMessage(`Use ${prefix}help to see the list of my commands.`);
-  });
-  
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    const aliases = command.aliases || [];    
-    
-      client.on("text", (data) => {
-          if (data.player.name === client.options.name) return;
-    
-          command.execute(data, client, prefix);
-
-          if (data.msg.startsWith(`${prefix}prefix`)) {
-              const newPrefix = data.msg.split(' ')[1]; // Assuming the new prefix is the second word after the !prefix command
-
-            if (typeof newPrefix !== "string") {
-              client.sendMessage('Error: Prefix is not defined.');
-              return;
-            }
-
-            if (newPrefix.match(/^[!@#$%^&*_\-+=:.<>/?\\]+$/) && newPrefix.length > 0) {
-                prefix = newPrefix; // Update the prefix
-                client.sendMessage(`Prefix updated to: ${prefix}`);
-            } else {
-                client.sendMessage('Please specify a new prefix after !prefix command. (no letters or numbers)');
-            }
-              return;
-          }
-    });
+    client.on("text", (data) => handleText(data, command));
+  }
 }
 
-  client.on("disconnect", (data) => {
-      console.log("Disconnected", data);
 
-      setTimeout(() => {
-        console.log('Rejoining in 3 seconds...');
-        main();
-      }, 3000)
-  });
+function handleConnect(client) {
+  console.log(`Connected to ${client.lobbyId}\nCurrent Player Count: ${client.players.length}`);
+  client.sendMessage(`Use ${prefix}help to see the list of my commands.`);
 }
+
+
+function handleDisconnect(data) {
+  console.log("Disconnected", data);
+  setTimeout(() => {
+    console.log('Rejoining in 3 seconds...');
+    main();
+  }, 3000);
+}
+
+
+function handleText(data, command) {
+  if (data.player.name === client.options.name) return;
+
+  command.execute(data, client, prefix);
+  if (data.msg.startsWith(`${prefix}prefix`)) {
+    updatePrefix(data.msg, client);
+  }
+}
+
+
+function updatePrefix(msg, client) {
+  const newPrefix = msg.split(' ')[1]; // Assuming the new prefix is the second word after the !prefix command
+
+  if (typeof newPrefix !== "string") {
+    client.sendMessage('Error: Prefix is not defined.');
+    return;
+  }
+
+  if (newPrefix.match(/^[!@#$%^&*_\-+=:.<>/?\\]+$/) && newPrefix.length > 0) {
+    prefix = newPrefix; // Update the prefix
+    client.sendMessage(`Prefix updated to: ${prefix}`);
+  } else {
+    client.sendMessage('Please specify a new prefix after !prefix command. (no letters or numbers)');
+  }
+}
+
+
+// ---
+
+
+function main() {
+  const client = setupClient();
+  registerEventHandlers(client);
+}
+
+
+// ---
+
 
 main();
