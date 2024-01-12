41c56eebe490e29dbc1c1851e58db21c379cc0ec
w0ahL <149548505+w0ahL@users.noreply.github.com>
2024-01-12 00:28:23 +0000

    Remove auto disconnect
    This is not needed really for a project like CommanderBox.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Client = require('./client/gameClient.js')
 function setupClient() {
   return new Client({
     name: "CommanderBox",
-    lobbyCode: 'zFCD76P5'
+    lobbyCode: ''
   });
 }
 
@@ -11,14 +11,6 @@ function main() {
   const client = setupClient();
   
   client.registerEventHandlers();
-  
-  client.on("disconnect", () => {
-    console.log('Rejoining in 3 seconds...');
-    
-    setTimeout(() => {
-      main();
-    }, 3000)
-  })
 }
 
 main();
